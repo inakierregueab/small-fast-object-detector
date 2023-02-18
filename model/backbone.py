3be5b1691b7dc623889b0034a91109b64114a03c
Inaki Erregue <inaki.erregue@gmail.com>
2023-02-18 13:21:50 +0100

    Begin model implementation, backbone

---
+++ model/backbone.py
@@ -0,0 +1,60 @@
+import torch.nn as nn
+import torch
+
+from model.common import CBL, C3
+
+
+class SPPF(nn.Module):
+    """
+    Spatial Pyramid Pooling Feature - Fast (SPPF) layer for YOLOv5 by Glenn Jocher
+    Parameters:
+        in_channels (int): number of channel of the input tensor
+        out_channels (int): number of channel of the output tensor
+    """
+    def __init__(self, in_channels, out_channels):
+        super(SPPF, self).__init__()
+
+        c_ = int(in_channels//2)
+
+        self.c1 = CBL(in_channels, c_, 1, 1, 0)
+        self.pool = nn.MaxPool2d(kernel_size=5, stride=1, padding=2)
+        self.c_out = CBL(c_ * 4, out_channels, 1, 1, 0)
+
+    def forward(self, x):
+        x = self.c1(x)
+        pool1 = self.pool(x)
+        pool2 = self.pool(pool1)
+        pool3 = self.pool(pool2)
+
+        # TODO: check concatenation
+        return self.c_out(torch.cat([x, pool1, pool2, pool3], dim=1))
+
+
+class CSPBackbone(nn.Module):
+    """
+    Parameters:
+        first_out (int): number of channel of the first output tensor
+    """
+    def __init__(self, first_out):
+        super(CSPBackbone, self).__init__()
+
+        self.backbone = nn.ModuleList()
+        self.backbone += [
+            CBL(in_channels=3, out_channels=first_out, kernel_size=6, stride=2, padding=2),
+            CBL(in_channels=first_out, out_channels=first_out * 2, kernel_size=3, stride=2, padding=1),
+            C3(in_channels=first_out * 2, out_channels=first_out * 2, width_multiple=0.5, depth=2),
+            CBL(in_channels=first_out * 2, out_channels=first_out * 4, kernel_size=3, stride=2, padding=1),
+            C3(in_channels=first_out * 4, out_channels=first_out * 4, width_multiple=0.5, depth=4),
+            CBL(in_channels=first_out * 4, out_channels=first_out * 8, kernel_size=3, stride=2, padding=1),
+            C3(in_channels=first_out * 8, out_channels=first_out * 8, width_multiple=0.5, depth=6),
+            CBL(in_channels=first_out * 8, out_channels=first_out * 16, kernel_size=3, stride=2, padding=1),
+            C3(in_channels=first_out * 16, out_channels=first_out * 16, width_multiple=0.5, depth=2),
+            SPPF(in_channels=first_out * 16, out_channels=first_out * 16)
+        ]
+
+    # TODO: modify backbone to return intermediate feature maps
+    def forward(self, x):
+        pass
+
+
+
---
+++ model/common.py
@@ -0,0 +1,109 @@
+import torch.nn as nn
+import torch
+
+
+class CBL(nn.Module):
+    """
+    Basic block of CSP-Darknet: Convolutional + BatchNorm + SiLU
+    Parameters:
+        in_channels (int): number of channel of the input tensor
+        out_channels (int): number of channel of the output tensor
+        kernel_size (int): size of the convolving kernel
+        stride (int): stride of the convolution
+        padding (int): zero-padding added to both sides of the input
+    """
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super(CBL, self).__init__()
+
+        conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False)
+        bn = nn.BatchNorm2d(out_channels, eps=1e-3, momentum=0.03)
+
+        self.cbl = nn.Sequential(
+            conv,
+            bn,
+            nn.SiLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.cbl(x)
+
+
+class Bottleneck1(nn.Module):
+    """
+    Parameters:
+        in_channels (int): number of channel of the input tensor
+        out_channels (int): number of channel of the output tensor
+        width_multiple (float): it controls the number of channels (and weights)
+                                of all the convolutions beside the
+                                first and last one. If closer to 0,
+                                the simpler the modelIf closer to 1,
+                                the model becomes more complex
+    """
+    def __init__(self, in_channels, out_channels, width_multiple=1):
+        super(Bottleneck1, self).__init__()
+        c_ = int(width_multiple*in_channels)
+        self.c1 = CBL(in_channels, c_, kernel_size=1, stride=1, padding=0)
+        self.c2 = CBL(c_, out_channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        return self.c2(self.c1(x)) + x
+
+
+class Bottleneck2(nn.Module):
+    """
+    Parameters:
+        in_channels (int): number of channel of the input tensor
+        out_channels (int): number of channel of the output tensor
+        width_multiple (float): it controls the number of channels (and weights)
+                                of all the convolutions beside the
+                                first and last one. If closer to 0,
+                                the simpler the modelIf closer to 1,
+                                the model becomes more complex
+    """
+
+    def __init__(self, in_channels, out_channels, width_multiple=1):
+        super(Bottleneck2, self).__init__()
+        c_ = int(width_multiple * in_channels)
+        self.c1 = CBL(in_channels, c_, kernel_size=1, stride=1, padding=0)
+        self.c2 = CBL(c_, out_channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        return self.c2(self.c1(x))
+
+
+class C3(nn.Module):
+    """
+    Basic module for a kind of CSP backbone (https://arxiv.org/pdf/1911.11929v1.pdf)
+    Parameters:
+        in_channels (int): number of channel of the input tensor
+        out_channels (int): number of channel of the output tensor
+        width_multiple (float): it controls the number of channels (and weights)
+                                of all the convolutions beside the
+                                first and last one. If closer to 0,
+                                the simpler the modelIf closer to 1,
+                                the model becomes more complex
+        depth (int): it controls the number of times the bottleneck (residual block)
+                        is repeated within the C3 block
+        backbone (bool): if True, self.seq will be composed by bottlenecks 1, if False
+                            it will be composed by bottlenecks 2 (check in the image linked below)
+        https://user-images.githubusercontent.com/31005897/172404576-c260dcf9-76bb-4bc8-b6a9-f2d987792583.png
+    """
+    def __init__(self, in_channels, out_channels, width_multiple=1, depth=1, backbone=True):
+        super(C3, self).__init__()
+        c_ = int(width_multiple*in_channels)
+
+        self.c1 = CBL(in_channels, c_, kernel_size=1, stride=1, padding=0)
+        self.c_skipped = CBL(in_channels,  c_, kernel_size=1, stride=1, padding=0)
+        if backbone:
+            self.seq = nn.Sequential(
+                *[Bottleneck1(c_, c_, width_multiple=1) for _ in range(depth)]
+            )
+        else:
+            self.seq = nn.Sequential(
+                *[Bottleneck2(c_, c_, width_multiple=1) for _ in range(depth)]
+            )
+        self.c_out = CBL(c_ * 2, out_channels, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        x = torch.cat([self.seq(self.c1(x)), self.c_skipped(x)], dim=1)
+        return self.c_out(x)
