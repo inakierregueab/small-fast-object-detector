76f5a06e0c1daac2cef99b69caab160b845d24ad
Inaki Erregue <inaki.erregue@gmail.com>
2023-02-21 20:25:06 +0100

    Add VOC2007 test

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -24,3 +24,9 @@ class VOCDetectionDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.VOCDetection(self.data_dir, year="2007", image_set='train', download=True)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+if __name__ == '__main__':
+    #data_load_mnist = MnistDataLoader(data_dir="data/mnist", batch_size=1, shuffle=True, validation_split=0.0, num_workers=1, training=True)
+    data_load_voc = VOCDetectionDataLoader(data_dir="./data/VOC2007", batch_size=1, shuffle=True, validation_split=0.0, num_workers=1, training=True)
+
