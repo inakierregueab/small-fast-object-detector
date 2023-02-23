5f83758e71f748685057ca8ff07cb07afc84f104
Inaki Erregue <inaki.erregue@gmail.com>
2023-02-23 16:12:26 +0100

    Refactor

--- model/customYOLOv5.py
+++ model/customYOLOv5.py
@@ -52,9 +52,9 @@ class YOLOv5m(BaseModel):
             conf_thres (float): confidence threshold
             iou_thres (float): IoU threshold
         Returns:
-            bboxes (list): list of 3 tensors (one per head) with shape (batch_size, detected_objects, 5 + class_idx)
+            clean_predictions (list): list of 3 tensors (one per head) with shape (batch_size, detected_objects, 5 + class_idx)
         """
-        bboxes = []
+        decoded_predictions = []
 
         # Iterate over every head/scale output
         for head in range(self.num_heads):
@@ -67,20 +67,11 @@ class YOLOv5m(BaseModel):
             # across all classes should be equal to 1.
             normalized_output = yolo_output[head].sigmoid()
 
-
-            # Obtain cxcy grid
-            x_grid = torch.arange(nx)
-            x_grid = x_grid.repeat(ny).reshape(ny, nx)
-            y_grid = torch.arange(ny).unsqueeze(0)
-            y_grid = y_grid.T.repeat(1, nx).reshape(ny, nx)
-            xy_grid = torch.stack([x_grid, y_grid], dim=-1)
-            # Per each anchor in the scale, create an identical grid of (x, y) coordinates
-            xy_grid = xy_grid.expand(1, naxs, ny, nx, 2)
-
+            # Obtain the grid of (x, y) coordinates for every anchor in the scale
+            xy_grid = self.make_xy_grids(naxs, nx, ny)
 
             # Obtain anchor grid for every anchor in the scale and undo the stride normalization in __init__
             anchor_grid = (self.anchors[head]*self.stride[head]).reshape((1, naxs, 1, 1, 2)).expand(1, naxs, ny, nx, 2)
-
 
             # Obtain the objectness score and the class prediction
             obj = normalized_output[..., 4:5]
@@ -89,28 +80,56 @@ class YOLOv5m(BaseModel):
             class_idx = torch.argmax(class_pred, dim=-1).unsqueeze(-1)
             # best_class = torch.argmax(class_pred*obj, dim=-1).unsqueeze(-1)
 
-
             # Compute the absolute coordinates of bboxes
             xy = (2 * (normalized_output[..., 0:2]) + xy_grid - 0.5) * self.stride[head]
             wh = ((2 * normalized_output[..., 2:4]) ** 2) * anchor_grid
 
-
             # Concat and append to other heads predictions
             head_output = torch.cat((class_idx, obj, xy, wh), dim=-1).reshape(bs, -1, 6)
-            bboxes.append(head_output)
+            decoded_predictions.append(head_output)
 
         # Concatenate the predictions of all heads, this should be equal to other method
         # Its shape should be (batch_size, N, 6), where N = sum_heads(anchor_per_head * grid_y_head * grid_x_head)
-        bboxes = torch.cat(bboxes, dim=1)
+        decoded_predictions = torch.cat(decoded_predictions, dim=1)
 
         # Apply NMS
-        bboxes = self.nms(bboxes, conf_thres=conf_thres, iou_thres=iou_thres)
+        clean_predictions = self.nms(decoded_predictions, conf_thres=conf_thres, iou_thres=iou_thres)
 
-        return bboxes
+        return clean_predictions
+
+    def make_xy_grids(self, naxs, nx, ny):
+        """
+        Creates a grid of (x, y) coordinates for every anchor in the scale.
+        Parameters:
+            naxs (int): number of anchors in the scale
+            nx (int): number of grid cells in the x direction
+            ny (int): number of grid cells in the y direction
+        """
+
+        # Obtain cxcy grid
+        x_grid = torch.arange(nx)
+        x_grid = x_grid.repeat(ny).reshape(ny, nx)
+        y_grid = torch.arange(ny).unsqueeze(0)
+        y_grid = y_grid.T.repeat(1, nx).reshape(ny, nx)
+        xy_grid = torch.stack([x_grid, y_grid], dim=-1)
+
+        # Per each anchor in the scale, create an identical grid of (x, y) coordinates
+        xy_grid = xy_grid.expand(1, naxs, ny, nx, 2)
+
+        return xy_grid
 
     def nms(self, batch_predictions: torch.Tensor, conf_thres: float = 0.0, iou_thres: float = 0.0):
         """
-        Applies non-maximum suppression to the predicted boxes.
+        Recives a batch of predictions and applies non-maximum suppression to each image in the batch. But first, it
+        filters out the predictions with a confidence score lower than the confidence threshold, and changes the
+        coordinates from (x_center, y_center, width, height) to (x_min, y_min, x_max, y_max).
+
+        Parameters:
+            batch_predictions (torch.Tensor): tensor with shape (batch_size, N, 6)
+            conf_thres (float): confidence threshold
+            iou_thres (float): IoU threshold
+        Returns:
+            batch_selected_predictions (list): list of tensors with shape (detected_objects, x0y0x1y1 + class_idx)
         """
         # TODO: Avoid for loop, play with pytorch filtering and slicing
         batch_selected_predictions = []
@@ -123,18 +142,13 @@ class YOLOv5m(BaseModel):
             mask = confidence_score >= conf_thres
             boxes = prediction[mask, 2:]
             scores = confidence_score[mask]
-            class_ids = class_idx[mask]
+            class_idx = class_idx[mask]
 
             # Convert the coordinates from (x_center, y_center, width, height) to (x_min, y_min, x_max, y_max)
-            x_center, y_center, width, height = boxes.unbind(dim=1)
-            x_min = x_center - 0.5 * width
-            y_min = y_center - 0.5 * height
-            x_max = x_center + 0.5 * width
-            y_max = y_center + 0.5 * height
-            boxes = torch.stack([x_min, y_min, x_max, y_max], dim=1)
+            boxes = self.coord_transf_xywh_xyxy(boxes)
 
             # Post confidence filtering
-            conf_filtered = torch.cat((boxes, scores.unsqueeze(1), class_ids.unsqueeze(1)), 1)
+            conf_filtered = torch.cat((boxes, scores.unsqueeze(1), class_idx.unsqueeze(1)), 1)
 
             # Apply NMS
             keep_indices = box_ops.nms(boxes, scores, iou_thres)
@@ -143,6 +157,22 @@ class YOLOv5m(BaseModel):
             batch_selected_predictions.append(conf_filtered[keep_indices])
 
         return batch_selected_predictions
+
+    def coord_transf_xywh_xyxy(self, boxes):
+        """
+        Converts the coordinates from (x_center, y_center, width, height) to (x_min, y_min, x_max, y_max)
+        Parameters:
+            boxes (torch.Tensor): tensor with shape (N, 4)
+        Returns:
+            boxes (torch.Tensor): tensor with shape (N, 4)
+        """
+        x_center, y_center, width, height = boxes.unbind(dim=1)
+        x_min = x_center - 0.5 * width
+        y_min = y_center - 0.5 * height
+        x_max = x_center + 0.5 * width
+        y_max = y_center + 0.5 * height
+        boxes = torch.stack([x_min, y_min, x_max, y_max], dim=1)
+        return boxes
 
 
 if __name__ == "__main__":
@@ -173,10 +203,14 @@ if __name__ == "__main__":
     assert out[1].shape == (batch_size, 3, image_height//16, image_width//16, nc + 5)
     assert out[2].shape == (batch_size, 3, image_height//32, image_width//32, nc + 5)
 
-    pout2 = model.postprocessing(out)
-
     print("Success!")
     print("feedforward took {:.2f} seconds".format(end - start))
+
+    start = time.time()
+    post_output = model.postprocessing(out)
+    end = time.time()
+
+    print("postprocessing took {:.2f} seconds".format(end - start))
 
     # Check model size
     print("Total parameters: {:.5f}M".format(sum(p.numel() for p in model.parameters()) / 1e6))
