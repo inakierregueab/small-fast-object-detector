e806e3bdf332fa8695db94f52febcb3d692ba0c2
Inaki Erregue <inaki.erregue@gmail.com>
2023-03-01 10:55:57 +0100

    Add some metrics

--- model/metric.py
+++ model/metric.py
@@ -18,3 +18,35 @@ def top_k_acc(output, target, k=3):
         for i in range(k):
             correct += torch.sum(pred[:, i] == target).item()
     return correct / len(target)
+
+
+def class_accuracy(output, target, threshold=0.5):
+    tot_class_preds, correct_class = 0, 0
+    tot_noobj, correct_noobj = 0, 0
+    tot_obj, correct_obj = 0, 0
+
+    num_heads = 3
+    with torch.no_grad():
+        for i in range(num_heads):
+            # target[i] = target[i].to(config.DEVICE)
+            obj = target[i][..., 0] == 1
+            noobj = target[i][..., 0] == 0
+
+            correct_class += torch.sum(torch.argmax(output[i][..., 5:][obj], dim=-1) == target[i][..., 5][obj])
+            tot_class_preds += torch.sum(obj)
+
+            obj_preds = torch.sigmoid(output[i][..., 0]) > threshold
+            correct_obj += torch.sum(obj_preds[obj] == target[i][..., 0][obj])
+            tot_obj += torch.sum(obj)
+            correct_noobj += torch.sum(obj_preds[noobj] == y[i][..., 0][noobj])
+            tot_noobj += torch.sum(noobj)
+
+        class_acc = (correct_class / (tot_class_preds + 1e-16)) * 100
+        noobj_acc = (correct_noobj / (tot_noobj + 1e-16)) * 100
+        obj_acc = (correct_obj / (tot_obj + 1e-16)) * 100
+
+        print(f"Class accuracy is: {class_acc:2f}%")
+        print(f"No obj accuracy is: {noobj_acc:2f}%")
+        print(f"Obj accuracy is: {obj_acc:2f}%")
+
+    return (class_acc, noobj_acc, obj_acc)
